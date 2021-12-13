2cde024674b810fe996a4ebd6373e7a257475103
Samarth Nasnodkar <70008729+Cooldude069@users.noreply.github.com>
2021-12-13 18:24:33 +0530

    Made the prefix case insensitive.

--- bot.py
+++ bot.py
@@ -60,6 +60,19 @@ async def on_ready():
     print("Bot online.")
 
 
+@client.event
+async def on_message(message: discord.Message):
+    prfx = get_prefix(client, message).lower()
+    if message.content.lower().startswith(prfx):
+        t = 0
+        if message.content[len(prfx)] == " ":
+            t = 1
+
+        message.content = message.content[:len(prfx)].lower() + message.content[len(prfx) + t:]
+
+    await client.process_commands(message)
+
+
 # @client.event
 # async def on_guild_join(guild: discord.Guild):
 #     supportServer = client.get_guild(900056168716701696)
