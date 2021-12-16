d1b24e87ddf9850101e20fa19b07428569b82fa4
Samarth Nasnodkar <70008729+Cooldude069@users.noreply.github.com>
2021-12-16 12:45:52 +0530

    new stuff

--- src/cogs/games/honeycomb.py
+++ src/cogs/games/honeycomb.py
@@ -1,11 +1,15 @@
 import discord
+import topgg
 from discord.ext import commands
 import asyncio
+from discord_components import *
 from src.constants.timeouts import honeycomb_reply_timeout
-from src.constants.scramble_words import words
-from src.constants.urls import bot_icon
+from src.constants.scramble_words import words, easy_words
+from src.constants.urls import bot_icon, bot_vote_url
+from src.constants.vars import TOPGG_TOKEN
 import random
 from src.constants.games import games
+
 
 def scramble(word) -> str:
     """Scrambles a word"""
@@ -21,10 +25,18 @@ def scramble(word) -> str:
     return scrambled_word
 
 
-async def honey_solo(client: commands.Bot, ctx: commands.Context, word: str, user: discord.User):
+async def honey_solo(dbl_client: topgg.DBLClient, client: commands.Bot, ctx: commands.Context, word: str,
+                     user: discord.User):
+    voted = await dbl_client.get_user_vote(user.id)
+    add_on_text = "Vote the bot to get an easier word using the button below and then dm `voted`."
+    vote_button = Button(label="Vote", url=bot_vote_url, style=ButtonStyle.URL)
     scrambled_word = scramble(word)
     await user.create_dm()
-    await user.dm_channel.send(f"Your word is `{scrambled_word}`. You have `{honeycomb_reply_timeout}s`")
+    if voted:
+        await user.dm_channel.send(f"Your word is `{scrambled_word}`. You have `{honeycomb_reply_timeout}s`")
+    else:
+        await user.dm_channel.send(f"Your word is `{scrambled_word}`. You have `{honeycomb_reply_timeout}s`"
+                                   f"\n{add_on_text}", components=[vote_button])
     try:
         msg = await client.wait_for('message', timeout=honeycomb_reply_timeout,
                                     check=lambda x: x.author == user and isinstance(x.channel, discord.DMChannel))
@@ -32,6 +44,10 @@ async def honey_solo(client: commands.Bot, ctx: commands.Context, word: str, use
         await ctx.send(f"{user.mention} Took too long to respond. Player Eliminated!")
         return None
     else:
+        if msg.content.lower() == "voted" and not voted:
+            _voted = await dbl_client.get_user_vote(user.id)
+            if _voted:
+                return await honey_solo(dbl_client, client, ctx, random.choice(easy_words), user)
         if msg.content.lower() == word.lower():
             await user.dm_channel.send("That is correct!")
             return user
@@ -47,10 +63,12 @@ async def honey_collected(client: commands.Bot, ctx: commands.Context, users: li
         color=discord.Colour.purple()
     )
 
+    dbl_client = topgg.DBLClient(client, TOPGG_TOKEN)
+
     embed.set_thumbnail(url=bot_icon)
     embed.set_footer(text="Game will begin in 10 seconds.")
     await ctx.send(embed=embed)
 
-    _passed = await asyncio.gather(*[honey_solo(client, ctx, random.choice(words), user) for user in users])
+    _passed = await asyncio.gather(*[honey_solo(dbl_client, client, ctx, random.choice(words), user) for user in users])
     passed = [user for user in _passed if user is not None]
     return passed
